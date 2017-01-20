9f8ee61cd267f65adcd1694842af69f8135c5150
wangwenhui <wangwenhui@jiaxincloud.com>
2017-01-20 18:33:21 +0800

    添加代码

---
+++ src/com/kafka/Sender.java
@@ -0,0 +1,30 @@
+package com.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+
+
+public class Sender {
+
+  public static void main(String[] args) {
+	  Properties props = new Properties();
+	  props.put("bootstrap.servers", "localhost:9092");
+	  props.put("acks", "all");
+	  props.put("retries", 0);
+	  props.put("batch.size", 16384);
+	  props.put("linger.ms", 1);
+	  props.put("buffer.memory", 33554432);
+	  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+	  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+	  Producer<String, String> producer = new KafkaProducer<>(props);
+	  for(int i = 0; i < 10; i++)
+	      producer.send(new ProducerRecord<String, String>("test2", "key1"+i, "value1"+i));
+	  producer.close();
+  }
+}
